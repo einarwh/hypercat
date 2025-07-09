5411f4d6d670c6a8bf98f908405042ee06649678
Einar W. Høst <einarwh@gmail.com>
2025-07-09 20:36:45 +0200

    test add, push

---
+++ Hypercat.Tests/AddTests.fs
@@ -0,0 +1,29 @@
+﻿module AddTests
+
+open System
+open Xunit
+open Cat
+
+let testAdd n1 n2 result = 
+    match pushInput (IntInput n1) [] with 
+    | Extension [ IntItem n1 ] ->
+        match pushInput (IntInput n2) [ IntItem n1 ] with 
+        | Extension [ IntItem n2; IntItem n1 ] ->
+            match pushInput (NameInput "add") [ IntItem n2; IntItem n1 ] with 
+            | Reduction [ IntItem result ] -> Assert.True(true)
+            | _ -> failwith "??"
+        | _ -> failwith "??"
+    | _ -> failwith "??"
+
+[<Fact>]
+let ``Add 2 and 1`` () =
+    testAdd 2 1 3
+
+[<Fact>]
+let ``Add 22 and 18`` () =
+    testAdd 22 18 40
+
+[<Fact>]
+let ``Add -5 and 5`` () =
+    testAdd -5 5 0
+
---
+++ Hypercat.Tests/PushIntTests.fs
@@ -0,0 +1,24 @@
+﻿module PushIntTests
+
+open System
+open Xunit
+open Cat
+
+[<Fact>]
+let ``Push 2 onto empty stack`` () =
+    let input = IntInput 2
+    let stack = []
+    match pushInput input stack with 
+    | Extension [ IntItem 2 ] ->  
+        Assert.True(true)
+    | _ -> failwith "??"
+
+[<Fact>]
+let ``Push 2 onto stack [ IntItem 1 ]`` () =
+    let input = IntInput 2
+    let stack = [ IntItem 1 ]
+    match pushInput input stack with 
+    | Extension [ IntItem 2; IntItem 1 ] ->  
+        Assert.True(true)
+    | _ -> failwith "??"
+
