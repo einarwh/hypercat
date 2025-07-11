63fc5f12bb96dd82d255a2a865ba586b887b5af2
Einar W. Høst <einarwh@gmail.com>
2025-07-11 15:37:37 +0200

    Update PushProcTests.fs

--- Hypercat.Tests/PushProcTests.fs
+++ Hypercat.Tests/PushProcTests.fs
@@ -8,26 +8,38 @@ open Cat
 let ``Push proc name onto empty stack`` () =
     let input = NameInput "proc"
     let stack = []
+    let expectedStack = [ ProcMarker ]
     match pushInput input stack with 
-    | Extension [ ProcMarker ] ->  
-        Assert.True(true)
+    | Extension actualStack ->  
+        Assert.Equal<Cat>(expectedStack, actualStack)
     | result -> failwith <| sprintf "%A ??" result
 
 [<Fact>]
 let ``Push proc name onto stack [ ProcMarker ]`` () =
     let input = NameInput "proc"
     let stack = [ ProcMarker ]
+    let expectedStack = [ ProcMarker; ProcMarker ]
     match pushInput input stack with 
-    | Extension [ ProcMarker; ProcMarker ] ->  
-        Assert.True(true)
+    | Extension actualStack ->  
+        Assert.Equal<Cat>(expectedStack, actualStack)
     | result -> failwith <| sprintf "%A ??" result
 
-// [<Fact>]
-// let ``Push end name onto stack [ ProcMarker; ProcMarker ]`` () =
-//     let input = NameInput "end"
-//     let stack = [ ProcMarker; ProcMarker ]
-//     match pushInput input stack with 
-//     | Extension [ NameItem "end"; ProcMarker; ProcMarker ] ->  
-//         Assert.True(true)
-//     | result -> failwith <| sprintf "%A ??" result
+[<Fact(Skip = "Doesn't work so.")>]
+let ``Push end name onto stack [ ProcMarker; ProcMarker ]`` () =
+    let input = NameInput "end"
+    let stack = [ ProcMarker; ProcMarker ]
+    let expectedStack = [ NameItem "end"; ProcMarker; ProcMarker ]
+    match pushInput input stack with 
+    | Extension actualStack ->  
+        Assert.Equal<Cat>(expectedStack, actualStack)
+    | result -> failwith <| sprintf "%A ??" result
 
+[<Fact(Skip = "Doesn't work so.")>]
+let ``Push end name onto stack [ NameInput "end"; ProcMarker; ProcMarker ]`` () =
+    let input = NameInput "end"
+    let stack = [ NameItem "end"; ProcMarker; ProcMarker ]
+    let expectedStack = [ ProcItem [ ProcItem [] ] ]
+    match pushInput input stack with 
+    | Extension actualStack ->  
+        Assert.Equal<Cat>(expectedStack, actualStack)
+    | result -> failwith <| sprintf "%A ??" result
